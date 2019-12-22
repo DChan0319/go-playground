a006234867472fff2653ff57d8f953f427d30dda
Darren Chan <darren@darchan0319@gmail.com>
2019-12-21 22:19:41 -0800

    Finished hello, again refactor

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Darren")
-	want := "Hello Darren, World"
 
-	if got != want {
-		t.Errorf("got: %q want: %q", got, want)
+	assertErrorMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got: %q want: %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to myself", func(t *testing.T) {
+		got := Hello("Darren")
+		want := "Hello Darren, World"
+
+		assertErrorMessage(t, got, want)
+	})
+
+	t.Run("if empty string is provided print out 'Hello, World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertErrorMessage(t, got, want)
+	})
 }
