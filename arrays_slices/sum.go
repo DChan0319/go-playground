02fd80cba3211068cbc958d2bfc7b9630baa89cc
Darren Chan <darren@darchan0319@gmail.com>
2019-12-25 18:32:26 -0800

    Finished arrays & slices section

---
+++ arrays_slices/sum.go
@@ -0,0 +1,39 @@
+package arrays
+
+// Sum func returns the sum of the integers in the array.
+func Sum(nums []int) int {
+	var sum int = 0
+
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+	return sum
+}
+
+// SumAll func sums every provided slice. returns []int where each element is the sum of each slice
+func SumAll(slicesToSum ...[]int) []int {
+  var sums []int
+
+  for _, numbers := range slicesToSum {
+    sums = append(sums, Sum(numbers))
+  }
+
+	return sums
+}
+
+// SumAllTails func similar to SumAll except doesn't include head in sum and empty slices.
+func SumAllTails(slicesToSum ...[]int) []int {
+  var sums []int
+
+  for _, numbers := range slicesToSum {
+    var sum int = 0
+
+    if len(numbers) > 0 {
+      sum = Sum(numbers[1:])
+    }
+    
+    sums = append(sums, sum)
+  }
+
+  return sums
+}
---
+++ arrays_slices/sum_test.go
@@ -0,0 +1,59 @@
+package arrays
+
+import "reflect"
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	t.Run("Collection of 5 numbers", func(t *testing.T) {
+		nums := []int{1, 2, 3, 4, 5}
+		var expected int = 15
+		var actual int = Sum(nums)
+
+		if expected != actual {
+			t.Errorf("Expected: %d, Actual: %d", expected, actual)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		var expected int = 6
+		var actual int = Sum(numbers)
+
+		if expected != actual {
+			t.Errorf("Expected: %d, Actual: %d", expected, actual)
+		}
+	})
+}
+
+func checkSums (t *testing.T, actual, expected []int) {
+  t.Helper()
+  
+  if !reflect.DeepEqual(actual, expected) {
+    t.Errorf("actual: %v expected: %v", actual, expected)
+  }
+}
+
+func TestSumAll(t *testing.T) {
+	expected := []int{3, 9}
+  actual := SumAll([]int{1, 2}, []int{0, 9})
+  
+  checkSums(t, actual, expected)
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("create sums of slices without tail", func (t *testing.T) {
+    actual := SumAllTails([]int{1, 2}, []int{0, 9})
+    expected := []int{2, 9}
+
+    checkSums(t, actual, expected)
+  })
+
+  t.Run("Safely sum empty slices and regular slices", func(t *testing.T) {
+    actual := SumAllTails([]int{}, []int{1,3,9})
+    expected := []int{0, 12}
+
+    checkSums(t, actual, expected)
+  })
+}
